77ab6562dbe1063702e48b07059418aa31162a51
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2016-04-11 16:42:48 +0800

    导出密钥

--- src/main/java/echizen/ryoma/KeyPair.java
+++ src/main/java/echizen/ryoma/KeyPair.java
@@ -11,41 +11,14 @@ public class KeyPair {
     private BigInteger PrivateKey;
     private ArrayList<BigInteger> PublicKey;
 
-    public void save(String PrivateKeyFileName, String PublicKeyFileName) throws IOException {
-        File file = new File(PrivateKeyFileName);
-        if (file.exists()) {
-            file.delete();
-        } else {
-            if (!file.getParentFile().exists()) {
-                file.getParentFile().mkdirs();
-            }
-            file.createNewFile();
-        }
-        Writer writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
-        writer.write("-----BEGIN FHE PRIVATE KEY-----\n");
-        writer.write((new BASE64Encoder()).encode((new Gson()).toJson(PrivateKey).toString().getBytes()));
-        writer.write("\n-----END FHE PRIVATE KEY-----\n");
-        writer.close();
-
-        file = new File(PublicKeyFileName);
-        if (file.exists()) {
-            file.delete();
-        } else {
-            if (!file.getParentFile().exists()) {
-                file.getParentFile().mkdirs();
-            }
-            file.createNewFile();
-        }
-        writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
-        writer.write("-----BEGIN FHE PUBLIC KEY-----\n");
-        writer.write((new BASE64Encoder()).encode((new Gson()).toJson(PublicKey).toString().getBytes()));
-        writer.write("\n-----END FHE PUBLIC KEY-----\n");
-        writer.close();
-    }
-
     public KeyPair() {
     }
 
+    public KeyPair(BigInteger secretKey, ArrayList<BigInteger> publicKey) {
+        PrivateKey = secretKey;
+        PublicKey = publicKey;
+    }
+
     public void save(String PrivateKeyFileName, String PublicKeyFileName) throws IOException {
         File file = new File(PrivateKeyFileName);
         if (file.exists()) {
@@ -76,9 +49,6 @@ public class KeyPair {
         writer.write((new BASE64Encoder()).encode((new Gson()).toJson(PublicKey).toString().getBytes()));
         writer.write("\n-----END FHE PUBLIC KEY-----\n");
         writer.close();
-    }    public KeyPair(BigInteger secretKey, ArrayList<BigInteger> publicKey) {
-        PrivateKey = secretKey;
-        PublicKey = publicKey;
     }
 
     public BigInteger getPrivateKey() {
--- src/main/java/echizen/ryoma/Main.java
+++ src/main/java/echizen/ryoma/Main.java
@@ -8,10 +8,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         FHE fhe = new FHE();
-        KeyPair keyPair = fhe.generateKeyPair(64);
+        KeyPair keyPair = fhe.generateKeyPair(128);
         ArrayList<BigInteger> encrypt = fhe.encrypt("1111111011111111111");
         System.out.println(fhe.decrypt(encrypt));
-
-        //keyPair.save("D:/test.key", "D:/test.crt");
+        keyPair.save("D:/test.key", "D:/test.crt");
     }
 }
