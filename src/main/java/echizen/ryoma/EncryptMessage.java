02f38eca3a1c627619831f8875d74ff2e32808ff
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2016-04-12 16:30:42 +0800

    实现全同态加密

---
+++ src/main/java/echizen/ryoma/EncryptMessage.java
@@ -0,0 +1,9 @@
+package echizen.ryoma;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class EncryptMessage {
+    public ArrayList<BigInteger> C;
+    public ArrayList<Integer> Z;
+}
