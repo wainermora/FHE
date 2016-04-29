71c1c0a0fd17030fc06e0570e3f4f23acc1e3698
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2016-04-29 17:02:03 +0800

    分离加密、解密

--- src/main/java/echizen/ryoma/Main.java
+++ src/main/java/echizen/ryoma/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        KeyPair FHE_KeyPair = new KeyPair(192);
+        KeyPair FHE_KeyPair = new KeyPair(128);
         FHE_KeyPair.generate();
 
         Encrypt FHE_Encrypt = new Encrypt(FHE_KeyPair.getPublicKey());
@@ -14,8 +14,8 @@ public class Main {
 
         FHE_KeyPair.save("D:/test.key", "D:/test.crt");
 
-        ArrayList<EncryptMessage> c1 = FHE_Encrypt.encrypt("00011");
-        ArrayList<EncryptMessage> c2 = FHE_Encrypt.encrypt("11000");
+        ArrayList<EncryptMessage> c1 = FHE_Encrypt.encrypt("0011");
+        ArrayList<EncryptMessage> c2 = FHE_Encrypt.encrypt("1100");
         System.out.println(FHE_Decrypt.decrypt(FHE_Encrypt.and(c1, c2)));
         System.out.println(FHE_Decrypt.decrypt(FHE_Encrypt.xor(c1, c2)));
         System.out.println(FHE_Decrypt.decrypt(FHE_Encrypt.add(c1, c2)));
